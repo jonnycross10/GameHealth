3963ab0ed08d2e2716fe45a878f003ddff94e786
Jonny Cross <crossjonny5@gmail.com>
2020-11-05 22:55:32 -0800

    the code works perfectly when big text is turned on in game

--- scrnShot.py
+++ scrnShot.py
@@ -5,16 +5,25 @@ except ImportError:
     import Image
 import pytesseract
 
+#normal text mode
+#the defect: (0,0,300,50)
+#the ironclad: (0,0,330,50)
+
+#big text mode:
+#the defect: (0,0,360,50)
+#the ironclad: (0,0,360,50)
+
+
 #region parameter: left, top, top, width, height
-screenshot = pyautogui.screenshot(region = (600,0,400,100))
+screenshot = pyautogui.screenshot(region = (0,0,360,50))
 screenshot.save("screen.png")
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-health = pytesseract.image_to_string(Image.open('screen.png'))#.strip()
+health = pytesseract.image_to_string(Image.open('screen.png')).strip()
 
 
-#health = health[-5:].strip(" ")
+health = health[-5:].strip(" ")
 
 print(health)
 f1 = open("health.txt", "w")
