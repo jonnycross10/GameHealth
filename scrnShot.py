d9d23ca445a340532e528c9f92a61cf7aee12711
Jonny Cross <crossjonny5@gmail.com>
2020-11-08 13:13:27 -0800

    it will now continuously update the file and close it

--- scrnShot.py
+++ scrnShot.py
@@ -13,33 +13,36 @@ import pytesseract
 #the defect: (0,0,360,50)
 #the ironclad: (0,0,360,50)
 
-
-#region parameter: left, top, top, width, height
-screenshot = pyautogui.screenshot(region = (0,0,360,50))
-screenshot.save("screen.png")
-
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
-health = pytesseract.image_to_string(Image.open('screen.png')).strip()
-
-
-health = health[-5:].strip(" ")
-
-print(health)
-
-
 def convertPercentage(fraction):
-	numerator = 0
-	denominator = 0
-	splitFraction = fraction.split("/")
-	numerator = int(splitFraction[0])
-	denominator = int(splitFraction[1])
-	percentage = numerator / denominator
-	#int gets rid of trailing 0, and * 100 turns it into a whole #
-	return(int(round(percentage,2)*100))
+		numerator = 0
+		denominator = 0
+		splitFraction = fraction.split("/")
+		numerator = int(splitFraction[0])
+		denominator = int(splitFraction[1])
+		percentage = numerator / denominator
+		#int gets rid of trailing 0, and * 100 turns it into a whole #
+		return(int(round(percentage,2)*100))
+#region parameter: left, top, top, width, height
+while True:
 
-print(convertPercentage(health))
+	screenshot = pyautogui.screenshot(region = (0,0,360,50))
+	screenshot.save("screen.png")
 
-f1 = open("health.txt", "w")
-f1.write(str(convertPercentage(health)))
+	pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+	health = pytesseract.image_to_string(Image.open('screen.png')).strip()
+
+
+	health = health[-5:].strip(" ")
+
+	print(health)
+
+
+	
+
+	print(convertPercentage(health))
+
+	f1 = open("health.txt", "w")
+	f1.write(str(convertPercentage(health)))
+	f1.close()
 
